98f85757b709dc88e67bfce1162ac31c9a85e8a2
Michael Gale <michael@michaelgale.dev>
2022-06-08 20:41:37 +1000

    feat: grid experiments

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,13 @@
 const colors = await $fetch('/api/citadel')
 </script>
 <template>
-  <div>
+  <div class="container">
     <h1>Citadel</h1>
+    <div class="filter">
+      <select name="" id="">
+        <option value=""></option>
+      </select>
+    </div>
     <div class="grid">
       <div class="card" v-for="(color, index) in colors.table" :key="`${color}--index`">
         <div class="card-swatch" :style="{'background-color': color.hex }"></div>
@@ -14,22 +19,43 @@ const colors = await $fetch('/api/citadel')
           <strong>Hex:</strong> {{ color.hex }}<br>
           <strong>Delta:</strong> {{ color.delta || 'N/A'}}
         </div>
+        <div class="divider" v-show="(index + 1) % 5 === 0"></div>
       </div>
     </div>
   </div>
 </template>
 
 <style>
+.divider {
+  position: absolute;
+  bottom: -0.75rem;
+  left: 0;
+  background-color: red;
+  width: 100%;
+  height: 7px;
+}
+.container { 
+  max-width: 1280px;
+}
 .grid {
   display: grid;
-  grid-template-columns: repeat(5, 1fr);
   grid-gap: 1rem;
 }
 .card {
+  position: relative;
   border: 1px solid gray;
   border-radius: 5px;
   display: grid;
-  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 3fr;
+}
+@media screen and (min-width: 768px) {
+  .grid {
+    grid-template-columns: repeat(5, 1fr);
+  }
+  .card {
+    grid-template-columns: unset;
+    grid-template-rows: repeat(2, 1fr);
+  }
 }
 .card-swatch {}
 .card-meta {
